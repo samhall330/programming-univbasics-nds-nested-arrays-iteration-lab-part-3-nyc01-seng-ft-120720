8b83495b963e036176f3016abb2639a8ec2e31b6
Sam Hall <mann.samantha11@gmail.com>
2020-11-29 19:46:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def join_nested_strings(src)
     src.each do |row|
       row.each do |element|
         if element.is_a?(String)
-        joined_strings += element
+        joined_strings << element
         end
   joined_strings
 end
